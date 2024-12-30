042b3b1b42bca219694beb09710f522032853a99
yousrakhan23 <ammarahkhan2016@gmail.com>
2024-12-30 05:10:26 +0500

    blog done

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,23 +3,23 @@ import Image from "next/image";
 import Blog from "@/app/Blog/page";
 export default function HeroSection() {
   return (
-    <div>
+    <div >
       <div className="relative  text-white h-screen flex items-center justify-center">
         {/* Background Image */}
-        <div className="absolute inset-0 justify-center items-center flex">
+        <div className="absolute justify-center items-center flex">
           <Image
             src="/images/hero_section_image.svg"
             alt="hero section"
             width={1216}
             height={500}
-            className="rounded-[12px] mx-auto object-cover shadow-2xl "
+            className="rounded-[12px] shadow-2xl "
           />
         </div>
-        <div className="absolute inset-0 "></div>
+       
 
         {/* Card Section */}
         <div className="relative max-w-6xl mx-auto p-6">
-          <div className="absolute top-8 right-8 bg-[#181A2A] rounded-lg shadow-lg p-6 w-96">
+          <div className="z-10 bg-[#181A2A] bg-opacity-70 p-6 rounded-lg shadow-lg">
             <div className="mb-4 text-sm font-medium text-blue-500">
               Technology
             </div>
